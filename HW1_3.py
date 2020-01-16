2e514cb475ccb9264664a09d7f7715d55a0047fd
DennicaN <58238184+DennicaN@users.noreply.github.com>
2020-01-17 01:56:49 +0500

    Revert "Create HW1_3.py"
    This reverts commit d2f076380fb1d5451f5d4f9d0654db2a2ad2f777.

--- HW1_3.py
+++
@@ -1,5 +0,0 @@
-num = int(input('Укажите цельночисловое значение '))
-num_2 = num * 10 + num
-num_3 = num * 100 + num * 10 + num
-answer = num + (num * 10 + num) + (num * 100 + num * 10 + num)
-print(f'Вы указали число:  {num}. Считаем {num} + {num_2} + {num_3} = {answer}')
